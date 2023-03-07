d3ff36423e72f4ca3e10f0ddaeb9b046c8225c38
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-06 21:28:43 -0300

    utilizando o state na lista das tarefas

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import lista from './Lista.module.scss';
 
 function Lista(){
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     },{
@@ -12,10 +12,17 @@ function Lista(){
     },{
         tarefa: 'Banco de dados',
         tempo: '02:00:00'
-    }]
+    }]);
     return (
         <aside className={lista.listaTarefas}>
-            <h2> Estudos do dia</h2>
+            <h2 onClick={() => {
+                console.log('h2 clicado', tarefas)
+                setTarefas([...tarefas, {tarefa: 'Boas práticas em C#', tempo: '05:00:00'}]);
+            }
+                 }> 
+                
+                Estudos do dia
+            </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item
