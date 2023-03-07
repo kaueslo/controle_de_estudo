92a6ccbb5213ba8aefaeb24fbb47dbb7366f568c
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-06 22:01:27 -0300

    incluindo uma tarefa no objeto

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,19 @@ import Botao from '../Button';
 import formulario from './Formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa : '',
+        tempo : '00:00'
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        console.log('state: ', this.state)
+    }
+
     render() {
         return(
-            <form className={formulario.novaTarefa}>
+            <form className={formulario.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={formulario.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
@@ -14,6 +24,8 @@ class Formulario extends React.Component {
                         type="text" 
                         name='tarefa'
                         id='tarefa'
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                         placeholder='O que você quer estudar?'
                         required
                     />
@@ -26,6 +38,8 @@ class Formulario extends React.Component {
                         type="time"
                         step='1'
                         name='tempo'
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                         id='tempo'
                         min='00:00:00'
                         max='04:00:00'
